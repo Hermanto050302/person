3fd67fe58cc814859053fcb321d17a777f794cfa
Hermanto050403 <hermantoimade45@gmail.com>
2019-04-20 23:18:55 +0800

    pertama

---
+++ index.php
@@ -0,0 +1,260 @@
+<?php include'header.php'?>
+<!--================Home Banner Area =================-->
+<section class="home_banner_area">
+    <div class="container box_1620">
+        <div class="banner_inner d-flex align-items-center">
+            <div class="banner_content">
+                <div class="media">
+                    <div class="d-flex">
+                        <img src="img/personal.jpg" alt="">
+                    </div>
+                    <div class="media-body">
+                        <div class="personal_text">
+                            <h6>Halo semua, saya</h6>
+                            <h3>I Made Hermanto</h3>
+                            <h4></h4>
+                            <p>Disini anda dapat mengenal saya lebih dekat dan saya akan membagikan pengalaman pribadi
+                                di website ini</p>
+                            <ul class="list basic_info">
+                                <li><a href="#"><i class="lnr lnr-calendar-full"></i> 5 maret 2002</a></li>
+                                <!-- <li><a href="#"><i class="lnr lnr-phone-handset"></i> 44 (012) 6954 783</a></li> -->
+                                <li><a href="#"><i class="lnr lnr-envelope"></i> hermantoimade45@gmail.com</a></li>
+                                <li><a href="#"><i class="lnr lnr-home"></i> Batubulan Kangin, Sukawati</a></li>
+                            </ul>
+                            <ul class="list personal_social">
+                                <li><a href="https://www.facebook.com/dto.gent"><i class="fa fa-facebook"></i></a></li>
+                                <li><a href="https://www.instagram.com/hermanto_imade/"><i
+                                            class="fa fa-instagram"></i></a></li>
+                                <!-- <li><a href="#"><i class="fa fa-linkedin"></i></a></li> -->
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--================End Home Banner Area =================-->
+
+<!--================Welcome Area =================-->
+<section id="about" class="welcome_area p_120">
+    <div class="container">
+        <div class="row welcome_inner">
+            <div class="col-lg-8">
+                <div class="welcome_text">
+                    <h4>Tentang saya</h4>
+                    <p class="text-danger">
+                        ----------------------------------------------------------------------------------
+                    </p>
+                    <p class="text-danger">-----------Konten ini sedang proses pengerjaan-----------</p>
+                    <p class="text-danger">
+                        ----------------------------------------------------------------------------------
+                    </p>
+                    <!-- <div class="row">
+                        <div class="col-md-4">
+                            <div class="wel_item">
+                                <i class="lnr lnr-database"></i>
+                                <h4>$2.5M</h4>
+                                <p>Total Donation</p>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="wel_item">
+                                <i class="lnr lnr-book"></i>
+                                <h4>1465</h4>
+                                <p>Total Projects</p>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="wel_item">
+                                <i class="lnr lnr-users"></i>
+                                <h4>3965</h4>
+                                <p>Total Volunteers</p>
+                            </div>
+                        </div>
+                    </div> -->
+                </div>
+            </div>
+            <!-- <div class="col-lg-6">
+                    <div class="tools_expert">
+                        <div class="skill_main">
+                            <div class="skill_item">
+                                <h4>After Effects <span class="counter">85</span>%</h4>
+                                <div class="progress_br">
+                                    <div class="progress">
+                                        <div class="progress-bar" role="progressbar" aria-valuenow="85"
+                                            aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="skill_item">
+                                <h4>Photoshop <span class="counter">90</span>%</h4>
+                                <div class="progress_br">
+                                    <div class="progress">
+                                        <div class="progress-bar" role="progressbar" aria-valuenow="90"
+                                            aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="skill_item">
+                                <h4>Illustrator <span class="counter">70</span>%</h4>
+                                <div class="progress_br">
+                                    <div class="progress">
+                                        <div class="progress-bar" role="progressbar" aria-valuenow="70"
+                                            aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="skill_item">
+                                <h4>Sublime <span class="counter">95</span>%</h4>
+                                <div class="progress_br">
+                                    <div class="progress">
+                                        <div class="progress-bar" role="progressbar" aria-valuenow="95"
+                                            aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="skill_item">
+                                <h4>Sketch <span class="counter">75</span>%</h4>
+                                <div class="progress_br">
+                                    <div class="progress">
+                                        <div class="progress-bar" role="progressbar" aria-valuenow="75"
+                                            aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div> -->
+        </div>
+    </div>
+</section>
+<!--================End Welcome Area =================-->
+
+<!--================My Tabs Area =================-->
+<section id="education" class="mytabs_area p_120">
+    <div class="container">
+        <div class="tabs_inner">
+            <ul class="nav nav-tabs" id="myTab" role="tablist">
+                <li class="nav-item">
+                    <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab"
+                        aria-controls="home" aria-selected="true">My Education</a>
+                </li>
+            </ul>
+            <div class="tab-content" id="myTabContent">
+                <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                    <ul class="list">
+                        <li>
+                            <span></span>
+                            <div class="media">
+                                <div class="d-flex">
+                                    <p>March 2017 to present</p>
+                                </div>
+                                <div class="media-body">
+                                    <h4>Colorlib</h4>
+                                    <p>Senior Web Developer <br />Santa monica, Los angeles</p>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <span></span>
+                            <div class="media">
+                                <div class="d-flex">
+                                    <p>March 2017 to present</p>
+                                </div>
+                                <div class="media-body">
+                                    <h4>Colorlib</h4>
+                                    <p>Senior Web Developer <br />Santa monica, Los angeles</p>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <span></span>
+                            <div class="media">
+                                <div class="d-flex">
+                                    <p>March 2017 to present</p>
+                                </div>
+                                <div class="media-body">
+                                    <h4>Colorlib</h4>
+                                    <p>Senior Web Developer <br />Santa monica, Los angeles</p>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--================End My Tabs Area =================-->
+
+<!--================Feature Area =================-->
+<!-- <section class="feature_area p_120">
+        <div class="container">
+            <div class="main_title">
+                <h2>offerings to my clients</h2>
+                <p>If you are looking at blank cassettes on the web, you may be very confused at the difference in
+                    price. You may see some for as low as $.17 each.</p>
+            </div>
+            <div class="feature_inner row">
+                <div class="col-lg-4 col-md-6">
+                    <div class="feature_item">
+                        <i class="flaticon-city"></i>
+                        <h4>Architecture</h4>
+                        <p>If you are looking at blank cassettes on the web, you may be very confused at the difference
+                            in price. You may see some for as low as $17 each.</p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="feature_item">
+                        <i class="flaticon-skyline"></i>
+                        <h4>Interior Design</h4>
+                        <p>If you are looking at blank cassettes on the web, you may be very confused at the difference
+                            in price. You may see some for as low as $17 each.</p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="feature_item">
+                        <i class="flaticon-sketch"></i>
+                        <h4>Concept Design</h4>
+                        <p>If you are looking at blank cassettes on the web, you may be very confused at the difference
+                            in price. You may see some for as low as $17 each.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section> -->
+<!--================End Feature Area =================-->
+
+<!--================Home Gallery Area =================-->
+<section id="galery" class="home_gallery_area p_120">
+    <div class="container">
+        <div class="main_title">
+            <h2>My Gallery</h2>
+            <p class="text-danger">Dalam proses pengerjaan</p>
+        </div>
+    </div>
+    <div class="container">
+        <div id="imginstagram" class="gallery_f_inner row imageGallery1">
+
+        </div>
+    </div>
+</section>
+<!--================End Home Gallery Area =================-->
+<?php include'testimoni.php'?>
+<?php include'footer.php'?>
+
+
+<script>
+    $(document).ready(function () {
+
+        $('#selector').pongstgrm({
+            accessId: 'YOUR-ACCESS-ID',
+            accessToken: 'YOUR-ACCESS-TOKEN'
+        });
+
+    });
+    $('#imginstagram').pongstgrm({
+        show: 'recent'
+    });
+</script>
